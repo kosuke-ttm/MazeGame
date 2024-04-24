54a7b0c3f94f67b0f4c49284e7e0f0311cfaf1bb
kosuke <221205102@ccmailg.meijo-u.ac.jp>
2024-04-24 22:25:59 +0900

    Add jump capability

--- Assets/move_player.cs
+++ Assets/move_player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class move_player : MonoBehaviour
 {
+    public float jumpPower;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60; // 60fps
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * jumpPower;
+        }
         if(Input.GetKey("up")) // ↑なら前(Z 方向)に 0.1 だけ進む
         {
             transform.position += transform.forward * 0.1f;
