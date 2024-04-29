a7eb20b3efac8e2b854c7bfb63ae7911f14b1cac
Souta Takiguchi <221205099@ccmailg.meijo-u.ac.jp>
2024-04-30 00:24:27 +0900

    諸々の変更
    ・プレイヤーの移動速度の変更
    ・プレイヤーの転倒防止を実装
    ・第一層と第二層の基盤を設置
    ・第一層のチュートリアルエリアを実装
    ・チュートリアルの表示を実装しようとして失敗，いったん放置
    ・アセットを使用して階段を実装

--- Assets/move_player.cs
+++ Assets/move_player.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class move_player : MonoBehaviour
 {
+    public float jumpPower;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60; // 60fps
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector3.up * jumpPower;
+        }
         if(Input.GetKey("up")) // ↑なら前(Z 方向)に 0.1 だけ進む
         {
-            transform.position += transform.forward * 0.1f;
+            transform.position += transform.forward * 0.08f;
         }
         if(Input.GetKey("down")) // ↓なら後ろ(Z 方向)に 0.1 だけ進む
         {
-            transform.position -= transform.forward * 0.1f;
+            transform.position -= transform.forward * 0.08f;
         }
         if(Input.GetKey("right")) // →なら Y 軸に 5 度回転する
         {
-            transform.Rotate(0f, 6.0f, 0f);
+            transform.Rotate(0f, 3.0f, 0f);
         }
         if(Input.GetKey("left")) // ←ならY 軸に-5 度回転する
         {
-            transform.Rotate(0f, -6.0f, 0f);
+            transform.Rotate(0f, -3.0f, 0f);
         }
     }
 }
