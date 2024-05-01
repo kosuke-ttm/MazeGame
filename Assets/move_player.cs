b06c08fc5b8dbfc07099b439cc3d3139799c3324
Soutacky <115775866+Soutacky@users.noreply.github.com>
2024-05-01 13:09:05 +0900

    移動方法変更

--- Assets/move_player.cs
+++ Assets/move_player.cs
@@ -6,6 +6,7 @@ public class move_player : MonoBehaviour
 {
     public float jumpPower;
     private Rigidbody rb;
+    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +19,35 @@ public class move_player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
             rb.velocity = Vector3.up * jumpPower;
+            isJumping = true;
         }
-        if(Input.GetKey("up")) // ↑なら前(Z 方向)に 0.1 だけ進む
+        if(Input.GetKey("w")) // ↑なら前(Z 方向)に 0.1 だけ進む
         {
             transform.position += transform.forward * 0.08f;
         }
-        if(Input.GetKey("down")) // ↓なら後ろ(Z 方向)に 0.1 だけ進む
+        if(Input.GetKey("s")) // ↓なら後ろ(Z 方向)に 0.1 だけ進む
         {
             transform.position -= transform.forward * 0.08f;
         }
-        if(Input.GetKey("right")) // →なら Y 軸に 5 度回転する
+        if(Input.GetKey("d")) // →なら Y 軸に 5 度回転する
         {
             transform.Rotate(0f, 3.0f, 0f);
         }
-        if(Input.GetKey("left")) // ←ならY 軸に-5 度回転する
+        if(Input.GetKey("a")) // ←ならY 軸に-5 度回転する
         {
             transform.Rotate(0f, -3.0f, 0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // wallタグに設定されているオブジェクトに衝突したらジャンプ復活
+        if(collision.gameObject.CompareTag("wall"))
+        {
+            isJumping = false;
         }
     }
 }
