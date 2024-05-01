12c94ed907753090bceb6cf18d968f5fb2943d12
Souta Takiguchi <221205099@ccmailg.meijo-u.ac.jp>
2024-05-01 13:50:33 +0900

    change

--- Assets/move_player.cs
+++ Assets/move_player.cs
@@ -6,7 +6,6 @@ public class move_player : MonoBehaviour
 {
     public float jumpPower;
     private Rigidbody rb;
-    private bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,35 +18,25 @@ public class move_player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = Vector3.up * jumpPower;
-            isJumping = true;
         }
-        if(Input.GetKey("w")) // ↑なら前(Z 方向)に 0.1 だけ進む
+        if(Input.GetKey("up")) // ↑なら前(Z 方向)に 0.1 だけ進む
         {
             transform.position += transform.forward * 0.08f;
         }
-        if(Input.GetKey("s")) // ↓なら後ろ(Z 方向)に 0.1 だけ進む
+        if(Input.GetKey("down")) // ↓なら後ろ(Z 方向)に 0.1 だけ進む
         {
             transform.position -= transform.forward * 0.08f;
         }
-        if(Input.GetKey("d")) // →なら Y 軸に 5 度回転する
+        if(Input.GetKey("right")) // →なら Y 軸に 5 度回転する
         {
             transform.Rotate(0f, 3.0f, 0f);
         }
-        if(Input.GetKey("a")) // ←ならY 軸に-5 度回転する
+        if(Input.GetKey("left")) // ←ならY 軸に-5 度回転する
         {
             transform.Rotate(0f, -3.0f, 0f);
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        // wallタグに設定されているオブジェクトに衝突したらジャンプ復活
-        if(collision.gameObject.CompareTag("wall"))
-        {
-            isJumping = false;
         }
     }
 }
