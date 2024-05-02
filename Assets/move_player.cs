afb96139d8edec3a5831a2f02aa829688ccbdbe1
Souta Takiguchi <221205099@ccmailg.meijo-u.ac.jp>
2024-05-02 23:33:11 +0900

    第一層の完成
    難易度確認しといて

--- Assets/move_player.cs
+++ Assets/move_player.cs
@@ -21,21 +21,21 @@ public class move_player : MonoBehaviour
     void Update()
     {
         // マウスの移動量を取得
-        float mx = Input.GetAxis("Mouse X");
-        float my = Input.GetAxis("Mouse Y");
+        float mx = Input.GetAxis("Mouse X")*4;
+        float my = Input.GetAxis("Mouse Y")*(-4);
         
         // X方向に一定量移動していれば横回転
         if (Mathf.Abs(mx) > 0.01f)
         {
             // 回転軸はワールド座標のY軸
-            transform.RotateAround(player.transform.position, Vector3.up, mx*6);
+            transform.RotateAround(player.transform.position, Vector3.up, mx);
         }
 
         // Y方向に一定量移動していれば縦回転
         if (Mathf.Abs(my) > 0.01f)
         {
             // 回転軸はカメラ自身のX軸
-            transform.RotateAround(player.transform.position, transform.right, my*(-1));
+            transform.RotateAround(player.transform.position, transform.right, my);
         }
 
         if(Input.GetKeyDown(KeyCode.Space))
