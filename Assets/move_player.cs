a87e030a3e849f6e956da384af716d565c966641
kosuke <221205102@ccmailg.meijo-u.ac.jp>
2024-05-22 14:46:34 +0900

    Revert "BGM編集"
    This reverts commit 7a16a3f88d5acc1e4c0cec5304eb580c4348e3fe.

--- Assets/move_player.cs
+++ Assets/move_player.cs
@@ -9,68 +9,20 @@ public class move_player : MonoBehaviour
     private bool isJumping = false;
     [SerializeField] GameObject player;
 
-    [SerializeField]  AudioSource source;
-    [SerializeField]  AudioClip clip1;
-    [SerializeField]  AudioClip clip2;
-    [SerializeField]  AudioClip clip3;
-    [SerializeField]  AudioClip clip4;
-    [SerializeField]  AudioClip clip5;
-
-    private int musicflag = 0;
-
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60; // 60fps
         rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // transformを取得
-        Transform myTransform = this.transform;
-
-        // 座標を取得
-        Vector3 pos = myTransform.position;
-
         // マウスの移動量を取得
         float mx = Input.GetAxis("Mouse X")*4;
         float my = Input.GetAxis("Mouse Y")*(-4);
-
-
-        if(pos.y < 16 && musicflag != 1)
-        {
-            source.Stop();
-            source.PlayOneShot(clip1);
-            musicflag = 1;
-        }
-        
-        if(16 <= pos.y && pos.y < 53 && musicflag != 2)
-        {
-            source.Stop();
-            source.PlayOneShot(clip2);
-            musicflag = 2;
-        }
-        if(53 <= pos.y && pos.y < 89 && musicflag != 3)
-        {
-            source.Stop();
-            source.PlayOneShot(clip3);
-            musicflag = 3;
-        }
-        if(89 <= pos.y && musicflag != 4)
-        {
-            source.Stop();
-            source.PlayOneShot(clip4);
-            musicflag = 4;
-        }
-        // if(89 <= pos.y && musicflag != 5)
-        // {
-        //     source.Stop();
-        //     source.PlayOneShot(clip5);
-        //     musicflag = 5;
-        // }
-
         
         // X方向に一定量移動していれば横回転
         if (Mathf.Abs(mx) > 0.01f)
@@ -85,7 +37,7 @@ public class move_player : MonoBehaviour
             // 回転軸はカメラ自身のX軸
             transform.RotateAround(player.transform.position, transform.right, my);
         }
-
+        
         if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
             rb.velocity = Vector3.up * jumpPower;
